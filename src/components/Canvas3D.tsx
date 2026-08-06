5635b0089599b2e88c05e944cc223231b254bf03
antalyailigunisi <antalyailigunisi@gmail.com>
2026-08-07 01:52:13 +0300

    Update Canvas3D.tsx

--- src/components/Canvas3D.tsx
+++ src/components/Canvas3D.tsx
@@ -12,7 +12,7 @@ function RealZirveGunisiModel() {
   const phone = '0506 252 16 81';
 
   // Fotoğraftaki Gerçek Çatı Eğim Açısı ve Tüp Boyu
-  const tubeAngle = Math.PI / 7.7; // ~66 derece dikey/eğimli duruş
+  const tubeAngle = Math.PI / 17.7; // ~66 derece dikey/eğimli duruş
   const tubeLength = 2.1;          // Tüp uzunluğu
 
   return (
